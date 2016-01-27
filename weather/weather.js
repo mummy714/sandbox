cb55d7446972bd1b0719929501b07d76634721d3
Mummert, Michael <mmummert@metlife.com>
2016-01-27 11:47:06 -0500

    Grab summary from forcast.io

--- weather/weather.js
+++ weather/weather.js
@@ -1,4 +1,15 @@
-var url = "";
+var url = "https://api.forecast.io/forecast/6d18c85388366ada86a47d2950700959/41.48954391,-75.70288086";
 function getWeatherData() {
-	
+	console.log("getWeatherData()");
+	$.ajax({
+		url: url,
+		type: "GET",
+		crossDomain: true,
+		dataType: "jsonp",
+		success: function(data) {
+			console.log(data);
+
+			$('#weatherData')[0].innerHTML = data.minutely.summary;
+		}
+	});
 }
